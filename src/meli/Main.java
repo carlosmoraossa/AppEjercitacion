2b3a225ce953bb83f881f893da699064ab07eeae
carlosmoraossa <carlos.mora@mercadolibre.com.co>
2021-01-21 11:43:30 -0500

    Ejercicio 2 Jorge Olivera y Carlos Mora

--- src/meli/Main.java
+++ src/meli/Main.java
@@ -6,12 +6,18 @@ public class Main {
 
     public static void main(String[] args) {
         // write your code here
-        System.out.println("Ingresa el numero");
+        System.out.println("Ingresa el numero n");
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
+
+        System.out.println("Ingrese el numero m");
+        Scanner scanner1 = new Scanner(System.in);
+        int m = scanner.nextInt();
         int i = 0;
+        int result = m/n;
+
         while (n > 0) {
-            if (i % 2 == 0 & i > 0) {
+            if (result%2  == 0 & i > 0) {
                 System.out.println( i + " Es par ");
                 n--;
             }
