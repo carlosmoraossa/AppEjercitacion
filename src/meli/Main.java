f9cb113df9d9ae2c6a0c608fb91f64128c67fefb
carlosmoraossa <carlos.mora@mercadolibre.com.co>
2021-01-21 13:46:48 -0500

    Ejercicio 5 Jorge Olivera y Carlos Mora

--- src/meli/Main.java
+++ src/meli/Main.java
@@ -4,37 +4,48 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
-
-        boolean band, a;
-        int numero, j = 0, s = 2, m;
-
-        System.out.println("Cuantos primos quieres generar");
-        Scanner entrada = new Scanner(System.in);
-        numero = entrada.nextInt();
-
-        for (int x = 1; x <= numero; x++) {
-
-            a = false;
-            while (!a) {
-
-                m = 2;
-                band = true;
-
-                while ((band) && (m < s)) {
-                    if(s % m == 0){
-                        band = false;
-                    } else {
-                        m = m +1;
-                    }
-                }
-                if(band){
-                    j = j +1;
-                    System.out.println("El numero primo "+j+" es "+s);
-                    a = true;
-                }
-                s = s+1;
+    private static int charCount(String s,char c){
+        int cont=0;
+        for(int i=0; i<s.length(); i++){
+            if(s.charAt(i)==c){
+                cont++;
             }
         }
+        return cont;
+    }
+
+    public static boolean cumpleCondicion(int i,int m, int d){
+        //convierto a string
+        String s = Integer.toString(i);
+        char c = (char) (d+'0');
+
+        return charCount(s,c)==m;
+
+    }
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ingrese cantidad de numeros que quiere ver: ");
+        int n = scanner.nextInt();
+
+        System.out.println("Ingrese cuantas veces se repite el numero: ");
+        int m = scanner.nextInt();
+
+        System.out.println("Ingrese cual es el numero que se repite: ");
+        int d = scanner.nextInt();
+
+        int i=1;
+        int cont=0;
+
+        while (cont<n){
+            if(cumpleCondicion(i,m,d)){
+                System.out.println(i);
+                cont++;
+            }
+            i++;
+        }
+        scanner.close();
     }
 }
