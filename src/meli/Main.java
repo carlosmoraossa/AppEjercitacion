797732433a5ed510c208a7ee715669dd6e6341cf
carlosmoraossa <carlos.mora@mercadolibre.com.co>
2021-01-21 12:40:00 -0500

    Ejercicio 4 Jorge Olivera y Carlos Mora

--- src/meli/Main.java
+++ src/meli/Main.java
@@ -5,15 +5,36 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        // write your code here
-        System.out.println("Ingresa el numero n");
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
 
-        if (n%2  != 0 ) {
-            System.out.println( n + " Es numero primo ");
-        } else {
-            System.out.println("No es numero primo");
+        boolean band, a;
+        int numero, j = 0, s = 2, m;
+
+        System.out.println("Cuantos primos quieres generar");
+        Scanner entrada = new Scanner(System.in);
+        numero = entrada.nextInt();
+
+        for (int x = 1; x <= numero; x++) {
+
+            a = false;
+            while (!a) {
+
+                m = 2;
+                band = true;
+
+                while ((band) && (m < s)) {
+                    if(s % m == 0){
+                        band = false;
+                    } else {
+                        m = m +1;
+                    }
+                }
+                if(band){
+                    j = j +1;
+                    System.out.println("El numero primo "+j+" es "+s);
+                    a = true;
+                }
+                s = s+1;
+            }
         }
     }
 }
